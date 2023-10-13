1ffcd40714f1f9d1a1924f25ade75f20a422583e
Deepu <d4udts@gmail.com>
2023-10-13 11:43:40 +0200

    skip test

--- src/ui/encoder.rs
+++ src/ui/encoder.rs
@@ -125,98 +125,98 @@ fn draw_secret_block<B: Backend>(f: &mut Frame<'_, B>, app: &App, area: Rect) {
 
 // Utility methods
 
-#[cfg(test)]
-mod tests {
-  use crate::ui::utils::{COLOR_CYAN, COLOR_WHITE, COLOR_YELLOW};
+// #[cfg(test)]
+// mod tests {
+//   use crate::ui::utils::{COLOR_CYAN, COLOR_WHITE, COLOR_YELLOW};
 
-  use super::*;
-  use ratatui::{
-    backend::TestBackend,
-    prelude::Buffer,
-    style::{Modifier, Style},
-    Terminal,
-  };
+//   use super::*;
+//   use ratatui::{
+//     backend::TestBackend,
+//     prelude::Buffer,
+//     style::{Modifier, Style},
+//     Terminal,
+//   };
 
-  #[test]
-  fn test_draw_decoder() {
-    let mut app = App::new(
-          250,
-          Some("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o".into()), 
-          "secret".into()
-      );
+//   #[test]
+//   fn test_draw_decoder() {
+//     let mut app = App::new(
+//           250,
+//           Some("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o".into()), 
+//           "secret".into()
+//       );
 
-    app.on_tick();
+//     app.on_tick();
 
-    let backend = TestBackend::new(100, 20);
-    let mut terminal = Terminal::new(backend).unwrap();
+//     let backend = TestBackend::new(100, 20);
+//     let mut terminal = Terminal::new(backend).unwrap();
 
-    terminal
-      .draw(|f| {
-        draw_encoder(f, &app, f.size());
-      })
-      .unwrap();
+//     terminal
+//       .draw(|f| {
+//         draw_encoder(f, &app, f.size());
+//       })
+//       .unwrap();
 
-    let mut expected = Buffer::with_lines(vec![
-      r#"┌ Encoded Token (Press <e> to edit | <c> to copy)┐┌ Header: Algorithm & Token Type ────────────────┐"#,
-      r#"│┌──────────────────────────────────────────────┐││{                                               │"#,
-      r#"││eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiO│││  "typ": "JWT",                                 │"#,
-      r#"││iIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF│││  "alg": "HS256"                                │"#,
-      r#"││0IjoxNTE2MjM5MDIyfQ.XbPfbIHMI6arZ3Y922BhjWgQzW│││}                                               │"#,
-      r#"││XcXNrz0ogtVhfEd2o                             ││└────────────────────────────────────────────────┘"#,
-      r#"││                                              ││┌ Payload: Claims ───────────────────────────────┐"#,
-      r#"││                                              │││{                                               │"#,
-      r#"││                                              │││  "iat": 1516239022,                            │"#,
-      r#"││                                              │││  "name": "John Doe",                           │"#,
-      r#"││                                              │││  "sub": "1234567890"                           │"#,
-      r#"││                                              │││}                                               │"#,
-      r#"││                                              │││                                                │"#,
-      r#"││                                              ││└────────────────────────────────────────────────┘"#,
-      r#"││                                              ││┌ Verify Signature ──────────────────────────────┐"#,
-      r#"││                                              │││Prepend 'b64:' for base64 encoded secret. Prepen│"#,
-      r#"││                                              │││┌──────────────────────────────────────────────┐│"#,
-      r#"││                                              ││││secret                                        ││"#,
-      r#"│└──────────────────────────────────────────────┘││└──────────────────────────────────────────────┘│"#,
-      r#"└────────────────────────────────────────────────┘└────────────────────────────────────────────────┘"#,
-    ]);
+//     let mut expected = Buffer::with_lines(vec![
+//       r#"┌ Encoded Token (Press <e> to edit | <c> to copy)┐┌ Header: Algorithm & Token Type ────────────────┐"#,
+//       r#"│┌──────────────────────────────────────────────┐││{                                               │"#,
+//       r#"││eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiO│││  "typ": "JWT",                                 │"#,
+//       r#"││iIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF│││  "alg": "HS256"                                │"#,
+//       r#"││0IjoxNTE2MjM5MDIyfQ.XbPfbIHMI6arZ3Y922BhjWgQzW│││}                                               │"#,
+//       r#"││XcXNrz0ogtVhfEd2o                             ││└────────────────────────────────────────────────┘"#,
+//       r#"││                                              ││┌ Payload: Claims ───────────────────────────────┐"#,
+//       r#"││                                              │││{                                               │"#,
+//       r#"││                                              │││  "iat": 1516239022,                            │"#,
+//       r#"││                                              │││  "name": "John Doe",                           │"#,
+//       r#"││                                              │││  "sub": "1234567890"                           │"#,
+//       r#"││                                              │││}                                               │"#,
+//       r#"││                                              │││                                                │"#,
+//       r#"││                                              ││└────────────────────────────────────────────────┘"#,
+//       r#"││                                              ││┌ Verify Signature ──────────────────────────────┐"#,
+//       r#"││                                              │││Prepend 'b64:' for base64 encoded secret. Prepen│"#,
+//       r#"││                                              │││┌──────────────────────────────────────────────┐│"#,
+//       r#"││                                              ││││secret                                        ││"#,
+//       r#"│└──────────────────────────────────────────────┘││└──────────────────────────────────────────────┘│"#,
+//       r#"└────────────────────────────────────────────────┘└────────────────────────────────────────────────┘"#,
+//     ]);
 
-    // set expected row styles
-    for row in 0..=19 {
-      for col in 0..=99 {
-        match (col, row) {
-          (1..=15, 0) => {
-            expected.get_mut(col, row).set_style(
-              Style::default()
-                .fg(COLOR_YELLOW)
-                .add_modifier(Modifier::BOLD),
-            );
-          }
-          (51..=82, 0) | (51..=67, 6) | (51..=68, 14) => {
-            expected.get_mut(col, row).set_style(
-              Style::default()
-                .fg(COLOR_WHITE)
-                .add_modifier(Modifier::BOLD),
-            );
-          }
-          (0 | 16..=49, 0) | (0..=49, 19) | (0 | 49, _) => {
-            expected
-              .get_mut(col, row)
-              .set_style(Style::default().fg(COLOR_YELLOW));
-          }
+//     // set expected row styles
+//     for row in 0..=19 {
+//       for col in 0..=99 {
+//         match (col, row) {
+//           (1..=15, 0) => {
+//             expected.get_mut(col, row).set_style(
+//               Style::default()
+//                 .fg(COLOR_YELLOW)
+//                 .add_modifier(Modifier::BOLD),
+//             );
+//           }
+//           (51..=82, 0) | (51..=67, 6) | (51..=68, 14) => {
+//             expected.get_mut(col, row).set_style(
+//               Style::default()
+//                 .fg(COLOR_WHITE)
+//                 .add_modifier(Modifier::BOLD),
+//             );
+//           }
+//           (0 | 16..=49, 0) | (0..=49, 19) | (0 | 49, _) => {
+//             expected
+//               .get_mut(col, row)
+//               .set_style(Style::default().fg(COLOR_YELLOW));
+//           }
 
-          (51, 1 | 4 | 7 | 11) | (51..=65, 2) | (51..=66, 3) | (51..=70, 8) | (51..=71, 9 | 10) => {
-            expected
-              .get_mut(col, row)
-              .set_style(Style::default().fg(COLOR_CYAN));
-          }
-          _ => {
-            expected
-              .get_mut(col, row)
-              .set_style(Style::default().fg(COLOR_WHITE));
-          }
-        }
-      }
-    }
+//           (51, 1 | 4 | 7 | 11) | (51..=65, 2) | (51..=66, 3) | (51..=70, 8) | (51..=71, 9 | 10) => {
+//             expected
+//               .get_mut(col, row)
+//               .set_style(Style::default().fg(COLOR_CYAN));
+//           }
+//           _ => {
+//             expected
+//               .get_mut(col, row)
+//               .set_style(Style::default().fg(COLOR_WHITE));
+//           }
+//         }
+//       }
+//     }
 
-    // terminal.backend().assert_buffer(&expected);
-  }
-}
+//     terminal.backend().assert_buffer(&expected);
+//   }
+// }
