bb6f0d6070bc8e527a7aa733dcc21f3dd4bca31b
Deepu <d4udts@gmail.com>
2023-10-24 14:24:02 +0200

    add tests for encoding

--- src/app/jwt_encoder.rs
+++ src/app/jwt_encoder.rs
@@ -71,15 +71,16 @@ pub fn encode_jwt_token(app: &mut App) {
     app.handle_error(String::from("Header should not be empty").into());
     return;
   }
+  let payload = app.data.encoder.payload.input.lines().join("\n");
+  if payload.is_empty() {
+    app.handle_error(String::from("Payload should not be empty").into());
+    return;
+  }
   let header: Result<Header, serde_json::Error> = serde_json::from_str(&header);
   match header {
     Ok(header) => {
       let alg = header.alg;
-      let payload = app.data.encoder.payload.input.lines().join("\n");
-      if payload.is_empty() {
-        app.handle_error(String::from("Payload should not be empty").into());
-        return;
-      }
+
       let payload: Result<Payload, serde_json::Error> = serde_json::from_str(&payload);
       match payload {
         Ok(payload) => {
@@ -128,5 +129,131 @@ pub fn encoding_key_from_secret(alg: &Algorithm, secret_string: &str) -> JWTResu
       true => EncodingKey::from_ed_pem(&secret).map_err(Error::into),
       false => Ok(EncodingKey::from_ed_der(&secret)),
     },
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use jsonwebtoken::{DecodingKey, Validation};
+  use tui_textarea::TextArea;
+
+  use super::*;
+  use crate::app::utils::slurp_file;
+
+  #[test]
+  fn test_encode_jwt_token_with_valid_payload_and_defaults() {
+    let mut app = App::new(250, None, "secrets".into());
+
+    app.data.encoder.payload.input = vec![
+      "{",
+      r#"  "sub": "1234567890","#,
+      r#"  "name": "John Doe","#,
+      r#"  "iat": 1516239022"#,
+      "}",
+    ]
+    .into();
+
+    encode_jwt_token(&mut app);
+
+    assert_eq!(app
+      .data
+      .encoder
+      .encoded
+      .get_txt(), "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTYyMzkwMjIsIm5hbWUiOiJKb2huIERvZSIsInN1YiI6IjEyMzQ1Njc4OTAifQ.TggX4VlPVD-2G5eUT5AhzepyMCx_nuzfiQ_YkdXsMKI");
+    assert!(app.data.encoder.signature_verified);
+  }
+
+  #[test]
+  fn test_encode_jwt_token_with_valid_payload_and_header_rs256() {
+    let mut app = App::new(250, None, "".into());
+
+    let header = vec!["{", r#"  "alg": "RS256","#, r#"  "typ": "JWT""#, "}"];
+    app.data.encoder.header.input = header.clone().into();
+
+    let claims = vec![
+      "{",
+      r#"  "sub": "1234567890","#,
+      r#"  "name": "John Doe","#,
+      r#"  "iat": 1516239022"#,
+      "}",
+    ];
+    app.data.encoder.payload.input = claims.clone().into();
+
+    app.data.encoder.secret.input = "@./test_data/test_rsa_private.pem".into();
+
+    encode_jwt_token(&mut app);
+    assert_eq!(app.data.error, "");
+    assert_eq!(app
+      .data
+      .encoder
+      .encoded
+      .get_txt(), "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1MTYyMzkwMjIsIm5hbWUiOiJKb2huIERvZSIsInN1YiI6IjEyMzQ1Njc4OTAifQ.a6yeSQkIfGD1Va9TgdImZUZ1AKO0OgP15ZFV4JPpZy8TpeByQpqUA3r2kJHNeUlETyEeYMKsDbZI5dYOEa_ZfF9xY6eslV1xmawOPkJYzf8IK3Lb42GEykn9qBWSvHzh5xFs2U1dYjJ9GW7bqhyPVaRVRKh1EBw8AbXmEYT42xSDnzkVUHhPpGM8_2anJNXvnexCQKlVRVVzZC04eHNsRIl5_n50irg7bQCO4z24kwViMTuCQTalV9LXCfdxp7_3Pp4Av_iJtkKHDXWs9GrrD6ttq1J6jOXDSbxn42XrPlxirr0pNtdvbk58W2LqYz4_G9q0HTRz_WO3FmaSxIxyqQ");
+    assert!(app.data.encoder.signature_verified);
+
+    // decode the key and verify
+    let mut secret_validator = Validation::new(Algorithm::RS256);
+    secret_validator.leeway = 1000;
+    secret_validator
+      .required_spec_claims
+      .retain(|claim| claim != "exp");
+    secret_validator.validate_exp = false;
+
+    let secret_string = "@./test_data/test_rsa_public.pem";
+
+    let secret = slurp_file(&secret_string.chars().skip(1).collect::<String>()).unwrap();
+
+    let decoded = jsonwebtoken::decode::<Payload>(
+      &app.data.encoder.encoded.get_txt(),
+      &DecodingKey::from_rsa_pem(&secret).unwrap(),
+      &secret_validator,
+    )
+    .unwrap();
+
+    assert_eq!(
+      decoded.header,
+      serde_json::from_str(header.join("\n").as_str()).unwrap()
+    );
+    assert_eq!(
+      decoded.claims,
+      serde_json::from_str(claims.join("\n").as_str()).unwrap()
+    );
+  }
+
+  #[test]
+  fn test_encode_jwt_token_with_empty_header() {
+    let mut app = App::new(250, None, "".into());
+
+    app.data.encoder.header.input = TextArea::default();
+
+    encode_jwt_token(&mut app);
+
+    assert_eq!(app.data.error, "Header should not be empty");
+  }
+
+  #[test]
+  fn test_encode_jwt_token_with_empty_payload() {
+    let mut app = App::new(250, None, "".into());
+
+    app.data.encoder.payload.input = TextArea::default();
+
+    encode_jwt_token(&mut app);
+
+    assert_eq!(app.data.error, "Payload should not be empty");
+  }
+
+  #[test]
+  fn test_encode_jwt_token_with_invalid_header() {
+    let mut app = App::new(250, None, "".into());
+
+    app.data.encoder.header.input = vec!["{", r#"  "sub": "1234567890""#, "}"].into();
+
+    app.data.encoder.payload.input = vec!["{", r#"  "sub": "1234567890""#, "}"].into();
+
+    encode_jwt_token(&mut app);
+
+    assert_eq!(
+      app.data.error,
+      "Error parsing header: missing field `alg` at line 3 column 1"
+    );
   }
 }
